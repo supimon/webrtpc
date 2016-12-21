bd75235688f898764b5f1c9848efbf94721deb47
Supi <supimon@gmail.com>
2016-12-21 06:39:16 +0530

    corrected the tick position for select elements and corrected the arguments array issue in the script

--- js/script.js
+++ js/script.js
@@ -15,14 +15,16 @@ $(function(){
             $('.modal-dialog').height($(window).height() - 2 * parseInt(modalMarginTop, 10));
         });
     }
-    window.validateUserForm = function(errorIDs) {
+    window.validateUserForm = function(errorIDs, showDataCheck) {
+        errorIDs = Array.prototype.slice.call(errorIDs); // errorIDs is by default treated as string
         resetValidations();
+        showDataCheck ? $('.form-group.data-check').show():$('.form-group.data-check').hide();
         var ERR_CLASSNAME;
         errorIDs.forEach(function(item, index){
-            item == 'inputPostal' ? ERR_CLASSNAME = 'has-code-error-feedback': ERR_CLASSNAME = 'has-error-feedback';
+            ERR_CLASSNAME = item == 'inputPostal' ?  'has-code-error-feedback': 'has-error-feedback';
             $('.form-group.'+item).addClass(ERR_CLASSNAME);
             validatedFields[item] = false;
-        })
+        });
         for (var key in validatedFields) {
             validatedFields[key] ? $('#'+key).addClass('tick') : '';
         }
